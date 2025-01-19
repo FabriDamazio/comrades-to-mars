b4524ca678f537f802dad73152a8d533046ef32f
FabriDamazio <fabridamazio@gmail.com>
2025-01-19 15:11:50 -0300

    Prototype the level and player movement

--- Player.cs
+++ Player.cs
@@ -1,15 +1,25 @@
 using Godot;
 
-public partial class Player : Node3D
+public partial class Player : RigidBody3D
 {
     public override void _Ready()
     {
-        GD.Print("Hello");
-        GD.Print("Dont Panic");
-        GD.Print(42);
     }
 
     public override void _Process(double delta)
     {
+        if (Input.IsActionPressed("ui_accept"))
+        {
+            ApplyCentralForce(Basis.Y * (float)delta * 1000.0f);
+        }
+        if (Input.IsActionPressed("ui_left"))
+        {
+            ApplyTorque(new Vector3(0, 0, 100.0f * (float)delta));
+        }
+
+        if (Input.IsActionPressed("ui_right"))
+        {
+            ApplyTorque(new Vector3(0, 0, -100.0f * (float)delta));
+        }
     }
 }
