9d98e7f20d4431b3360de41ab6184e8c6c9426c2
FabriDamazio <fabridamazio@gmail.com>
2025-01-20 18:13:34 -0300

    Create new input maps for player movement

--- Player.cs
+++ Player.cs
@@ -8,16 +8,16 @@ public partial class Player : RigidBody3D
 
     public override void _Process(double delta)
     {
-        if (Input.IsActionPressed("ui_accept"))
+        if (Input.IsActionPressed("boost"))
         {
             ApplyCentralForce(Basis.Y * (float)delta * 1000.0f);
         }
-        if (Input.IsActionPressed("ui_left"))
+        if (Input.IsActionPressed("rotate_left"))
         {
             ApplyTorque(new Vector3(0, 0, 100.0f * (float)delta));
         }
 
-        if (Input.IsActionPressed("ui_right"))
+        if (Input.IsActionPressed("rotate_right"))
         {
             ApplyTorque(new Vector3(0, 0, -100.0f * (float)delta));
         }
